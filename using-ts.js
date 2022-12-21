8970fbe9a6fb19a0464df8b6163faa2146ce1376
Olivia <oliviatmanalastas@gmail.com>
2022-12-21 09:25:48 -0800

    compiled ts

--- using-ts.js
+++ using-ts.js
@@ -5,5 +5,5 @@ var add = function (num1, num2) {
     return num1 + num2;
 };
 button.addEventListener("click", function () {
-    console.log(add(input1.value, input2.value));
+    console.log(add(+input1.value, +input2.value));
 });
